e90cf7e3826c2e1b7d7ab786d373d564160a65f5
Tim <tim.hargreaves@icloud.com>
2021-04-09 14:17:12 +0100

    Update K-D tree approach to any k

--- python/implementations/kdtree.py
+++ python/implementations/kdtree.py
@@ -16,7 +16,7 @@ class KDTree(Base):
         """See Base.predict"""
         pred = []
         for x0 in X_test:
-            neighbours = [self._nearest_neighbours(self.root, x0, K)]
+            neighbours = self._nearest_neighbours(self.root, x0, K)
             pred.append(sum(n.value for n in neighbours) / K)
         return pred
 
@@ -46,13 +46,12 @@ class KDTree(Base):
 
         return parent
 
-    # Todo: expand for K > 1
     def _nearest_neighbours(self, node, x_test, K, depth=0):
         # Constants
         dim = depth % 2
         # Special case
         if node is None:
-            return None
+            return [None for __ in range(K)]
         # Recursion
         if x_test[dim] < node.point[dim]:
             target = node.left
@@ -60,18 +59,19 @@ class KDTree(Base):
         else:
             target = node.right
             other = node.left
-        target_candidate = self._nearest_neighbours(target, x_test,
-                                                    K, depth + 1)
+        target_candidates = self._nearest_neighbours(target, x_test,
+                                                     K, depth + 1)
         # Compare to current node
-        candidate, r2 = KDTree._closest(target_candidate, node, x_test)
+        candidates = KDTree._insert(target_candidates, node, x_test)
         # Consider other branch
+        c = target_candidates[K-1]
         d2 = (node.point[dim] - x_test[dim]) ** 2
-        if r2 >= d2:
-            other_candidate = self._nearest_neighbours(other, x_test,
-                                                       K, depth + 1)
-            candidate, __ = KDTree._closest(other_candidate, candidate, x_test)
+        if c is None or sum((x1 - x0)**2 for x0, x1 in zip(c.point, x_test)) >= d2:
+            other_candidates = self._nearest_neighbours(other, x_test,
+                                                        K, depth + 1)
+            candidates = KDTree._merge(other_candidates, candidates, x_test, K)
 
-        return candidate
+        return candidates
 
     @staticmethod
     def _closest(n1, n2, x_test):
@@ -84,6 +84,53 @@ class KDTree(Base):
         d1 = sum((x1 - x0)**2 for x0, x1 in zip(n1.point, x_test))
         d2 = sum((x2 - x0)**2 for x0, x2 in zip(n2.point, x_test))
         return (n1, d1) if d1 < d2 else (n2, d2)
+
+    @staticmethod
+    def _insert(candidates, n, x_test):
+        if n is None:
+            return candidates
+        dn = sum((x1 - x0) ** 2 for x0, x1 in zip(n.point, x_test))
+        replace = False
+        for k in range(len(candidates)):
+            if candidates[k] is None:
+                replace = True
+            else:
+                c = candidates[k]
+                d = sum((x1 - x0) ** 2 for x0, x1 in zip(c.point, x_test))
+                if dn < d:
+                    replace = True
+            if replace:
+                candidates.insert(k, n)
+                del candidates[len(candidates) - 1]
+                break
+        return candidates
+
+    @staticmethod
+    def _merge(candidates1, candidates2, x_test, K):
+        dists1 = [
+            sum((x1 - x0) ** 2 for x0, x1 in zip(n.point, x_test)) if n else None
+            for n in candidates1
+        ]
+        dists2 = [
+            sum((x1 - x0) ** 2 for x0, x1 in zip(n.point, x_test)) if n else None
+            for n in candidates2
+        ]
+        i, j = 0, 0
+        candidates = []
+        for k in range(K):
+            if dists1[i] is None:
+                candidates.extend(candidates2[j:j+(K-k)])
+                break
+            elif dists2[j] is None:
+                candidates.extend(candidates1[i:i + (K - k)])
+                break
+            if dists1[i] < dists2[j]:
+                candidates.append(candidates1[i])
+                i += 1
+            else:
+                candidates.append(candidates2[j])
+                j += 1
+        return candidates
 
     @staticmethod
     def _argsort(seq):

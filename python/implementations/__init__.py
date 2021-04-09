4d8953049deade2063d30ff25ce136f10c73d716
Tim <tim.hargreaves@icloud.com>
2021-04-09 14:21:04 +0100

    Update implementations list

--- python/implementations/__init__.py
+++ python/implementations/__init__.py
@@ -1,7 +1,6 @@
 from naive import Naive
 from standard import Standard
 from triangle import TriangleInequality
-from grid import Grid
 from kdtree import KDTree
 
 implementations = [Naive, Standard, TriangleInequality, KDTree]
